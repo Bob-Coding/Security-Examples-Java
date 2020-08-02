54b978865037750b101842699f457b910c18361f
Bob-Coding <bobgunsing@gmail.com>
2020-08-02 17:41:48 +0200

    created a new user in memory db with role admin

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +24,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {    //Service how you retrieve your users from the database
+        UserDetails bobUser = User.builder()                //For now we create a new user instead of retrieving from database
+                .username("Bob")
+                .password("password")
+                .roles("ADMIN")                             //ROLE_ADMIN authentication role
+                .build();
+        return new InMemoryUserDetailsManager(bobUser);
     }
 }
