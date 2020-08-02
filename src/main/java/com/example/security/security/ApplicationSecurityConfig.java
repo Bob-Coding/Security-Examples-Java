77894850b72bcb776cb31816a0fca2fc4d6e57a0
Bob-Coding <bobgunsing@gmail.com>
2020-08-02 17:52:50 +0200

    added BCrypt encoder class and linked to config for usage

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }                                               // Enable Password encoder from PasswordConfig(BCrypt)
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {    //Service how you retrieve your users from the database
         UserDetails bobUser = User.builder()                //For now we create a new user instead of retrieving from database
                 .username("Bob")
-                .password("password")
+                .password(passwordEncoder.encode("password"))  //Encode password
                 .roles("ADMIN")                             //ROLE_ADMIN authentication role
                 .build();
         return new InMemoryUserDetailsManager(bobUser);
